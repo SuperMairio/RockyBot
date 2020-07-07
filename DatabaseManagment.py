d7e0a709390f103670b2580524fe1e1c289d4365
Mairi <mairi.mcqueer@gmail.com>
2020-07-07 15:15:01 +0100

    Add sqlite database and managment script

---
+++ DatabaseManagment.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+conn = sqlite3.connect('Usernames.db')
+x = 0
+
+def CreateDB():
+    conn.execute("CREATE TABLE IF NOT EXSISTS Usernames(NAME TEXT, ADMIN BOOL);")
+
+def EditDB(x):
+    while(x == 0):
+        userInput = input("What would you like to do?\n(a= Add new name, c= Change privileges, d= Delete name) ")
+        name = input('Username: ')
+        
+        if(userInput == 'a'):
+            admin = input('Privilege level (1= admin, 0= default): ')
+            conn.execute('INSERT INTO IF NOT EXISTS Usernames ((NAME, ADMIN) VALUES (?,?)'(name, admin))
+            conn.commit()
+            x = 1            
+        elif(userInput == 'c'):
+            changePriv = input('What would you like to change their privilege to?\n(1= admin, 0= default):')
+            conn.execute('UPDATE Username set ADMIN = (?) WHERE NAME = (?)'(changePriv,name))
+            conn.commit()
+            x = 1
+        elif(userInput == 'd'):
+            conn.execute("DELETE FROM Username WHERE NAME = (?)"(name))
+            conn.commit()
+            x = 1
+        else:
+            print('That is not a recognised command')
+
+def ViewDB():
+    cursor = conn.execute("SELECT * FROM Username;")
+    for(row in cursor):
+        print("Username:", row[0])
+        print("Admin:", row[1])
+
+
+while(x == 0):
+    userInput = input("What would you like to do?\n(c= Create, e= Edit, v= View) ")
+
+    if(userInput == 'c'):
+        CreateDB()
+        x = 1 
+    elif(userInput == 'e'):
+        EditDB(0)
+        x = 1
+    elif(userInput == 'v'):
+        ViewDB()
+        x = 1
+    else:
+        print('That is not a recognised command')
+
+conn.close()
--- TwitterBot.py
+++ TwitterBot.py
@@ -1,15 +1,35 @@
 import os
-# This contains my API variables, feel free to insert your own
+# This contains my API variables, make sure to insert your own
 from topsecretfile import *
 from twython import Twython, TwythonError
+import sqlite3
+import shutil
 
 if KEY == 'abc':
     print("It looks like you haven't set your API keys\nYou can get them from: https://developer.twitter.com/en/apps")
+    exit()
 
 twitter = Twython(KEY, SECRET)
 
 #send_direct_message
 
-def GetNextImage():
-    for photo in os.listdir(PHOTO_PATH):
-        return(photo)
+def Get_Image(): 
+    try:
+        for photo in os.listdir(PHOTO_PATH):
+    except(SystemError):
+        for photo in os.listdir(USED_PHOTO_PATH)
+
+    return(photo)
+
+def Send_Image():
+    image = Get_Image()
+
+    response = twitter.upload_media(media=image ,media_type='image/jpeg', media_category='dmImage')
+
+    shutil.move(image, USED_PHOTO_PATH)
+    
+def Get_Usernames():
+    # sqlite code here
+
+Send_Image()
+exit()
