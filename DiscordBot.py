adedd577b95c7ba411da8ba65ff273ac85db22fa
Mairi <mairi.mcqueer@gmail.com>
2020-07-29 17:05:01 +0100

    hardcoded file paths changed token gathering method minor logic changes WIP

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,46 +1,61 @@
 import os
 import random
+import shutil
 import discord
 from discord.ext import commands
-from topsecretfile.py import *
 
-rockyBot = commands.Bot(command_prefix = "/") #Character to prefix commands so it knows im giving it a command
+TOKEN = os.getenv("DISCORDTOKEN")
+PHOTO_PATH = "/home/mairi/Documents/RockyBot/Rocky"
+USED_PHOTO_PATH = "/home/mairi/Documents/RockyBot/OldiesButGoldies"
 
+
+rockyBot = commands.Bot(command_prefix = "/")
 msgList = [
     "Im sad",
     "I miss Rocky",
-    "Im stressed"
+    "Im stressed",
+    "I want to see my boy",
+    "test"
 ]
 replyList = [
     "Don't be sad heres another photo!",
     "Hope this helps!",
-    "Don't fur-get to take breaks!",
     "Rocky is proud of you, keep going!"
 ]
+channel = rockyBot.get_channel("general")
 
 @rockyBot.event
-def on_ready():
+async def on_ready():
     print("RockyBot is online!")
 
-def on_message(message):
+@rockyBot.event
+async def on_message(message):
+    photo = Get_Photo()
+
     if message.content in msgList:
         reply = random.choice(replyList)     
-        await message.channel.send(reply)
-        send_Photo(Get_Photo)
-
+        #await message.channel.send(reply)
+        print(PHOTO_PATH+photo)
+        await channel.send(file=discord.File((PHOTO_PATH+photo)))
+    
+    # shutil.move(PHOTO_PATH+photo, USED_PHOTO_PATH)
 
 def Get_Photo(): 
     try:
-        photos = os.listdir(PHOTO_PATH):
+        photos = os.listdir(PHOTO_PATH)
     except(SystemError): #TODO: Check this is the only error
         photos = os.listdir(USED_PHOTO_PATH)
     
     photo = random.choice(photos)
+    print(photo)
     return(photo)
 
-def send_Photo(photo):
-    with open(photo, 'rb') as pic:
-        catPic = discord.File(pic)
-        await channel.send(catPic)
+#@rockyBot.event
+#async def send_Photo(photo):
+#    with open(photo, 'rb') as pic:
+#        catPic = discord.File(pic)
+#        await channel.send(catPic)
+    
+#    shutil.move(photo, USED_PHOTO_PATH)
 
 rockyBot.run(TOKEN)
