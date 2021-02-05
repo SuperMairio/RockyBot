e9be654354c830e49507068c535b93bedcc9bdd8
1320071 <1700231@uad.ac.uk>
2021-02-05 16:44:45 +0000

    added: - help command - made commands case insensitive - more encouraging messages

--- DiscordBot.py
+++ DiscordBot.py
@@ -19,33 +19,53 @@ if not os.path.isdir(PHOTO_PATH) or not os.path.isdir(USED_PHOTO_PATH):
 
 rockyBot = commands.Bot(command_prefix = "/")
 msgList = [
-    "Im sad",
-    "I miss Rocky",
-    "Im stressed",
-    "I want to see my boy",
-    "Where is my son",
-    "test"
+    "i miss rocky",
+    "i want to see my boy",
+    "where is my son",
+    "f",
+    "stressed"
+    "upset",
+    "sad",
+    "give up",
+    "cat",
+    "rocky"
 ]
 replyList = [
     "Don't be sad heres another photo!",
     "Hope this helps!",
-    "Rocky is proud of you, keep going!"
+    "Rocky is proud of you, keep going!",
+    "Never give up!",
+    "Rocky says just nap and it'll be ok"
 ]
+
+helpMsg = "** RockyBot **\nUse `rocky help` to bring this up again\nSentences/keywords to summon Rocky (case insensitive):"
+for msg in msgList:
+    helpMsg += "\n" 
+    helpMsg += msg
 
 @rockyBot.event
 async def on_ready():
     print("RockyBot is online!")
+    channel = rockyBot.get_channel(CHANNELID) 
+    await channel.send(helpMsg)
 
 @rockyBot.event
 async def on_message(message):
     photo = Get_Photo()
+    msg = message.content
 
-    if message.content in msgList:
-        reply = random.choice(replyList)  
-        await rockyBot.wait_until_ready()
-        channel = rockyBot.get_channel(CHANNELID)  
-        await message.channel.send(reply,file=discord.File(PHOTO_PATH+"/"+photo))
-        shutil.move(PHOTO_PATH+"/"+photo, USED_PHOTO_PATH)
+    for item in msgList:
+        if item in msg.lower():
+            reply = random.choice(replyList)  
+            await rockyBot.wait_until_ready()
+            channel = rockyBot.get_channel(CHANNELID)  
+            await message.channel.send(reply,file=discord.File(PHOTO_PATH+"/"+photo))
+            shutil.move(PHOTO_PATH+"/"+photo, USED_PHOTO_PATH)
+            break # stops 5 messages being sent
+        elif msg.lower() == "rocky help":
+            await rockyBot.wait_until_ready()
+            channel = rockyBot.get_channel(CHANNELID)  
+            await message.channel.send(helpMsg)
 
 def Get_Photo(): 
     photos = os.listdir(PHOTO_PATH)
